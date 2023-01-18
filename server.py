9de56bfc122c8ccb9b9438a1f25674eb4091f89a
Ismarji Workman <ismarjiw@protonmail.com>
2023-01-18 16:25:51 -0500

    added back insecure transport

--- server.py
+++ server.py
@@ -37,7 +37,7 @@ FORTUNES = [
 
 OPEN_WEATHER_API_KEY = os.environ.get('OPEN_WEATHER_API_KEY')
 
-# os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
 client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
 
